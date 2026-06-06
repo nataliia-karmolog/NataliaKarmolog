061775739087dbace73abd80f794d1d559ee39ff
nataliia-karmolog <140195669+nataliia-karmolog@users.noreply.github.com>
2026-06-06 21:48:36 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://demqn96.github.io',
-  base: '/NataliaKarmolog_new_course',
+  site: 'https://nataliia-karmolog.github.io/',
+  base: '/NataliaKarmolog',
   vite: {
     plugins: [tailwindcss()],
   },
